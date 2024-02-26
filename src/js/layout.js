532694c68c935d57a1466a955dae9fa9f0be9519
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-02-26 09:18:16 +0000

    deleted unnecessary files

--- src/js/layout.js
+++ src/js/layout.js
@@ -2,11 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Navbar } from "./component/navbar";
 import { Home } from "./views/home";
 import { AddContact } from "./views/addContact";
-import { Demo } from "./views/demo";
-import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 
@@ -20,12 +17,9 @@ const Layout = () => {
 		<div>
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Navbar />
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/add" element={<AddContact />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
 						<Route 
 							path="*" 
 							element={
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ContactCard from "./contactCard";
+import ContactCard from "../component/contactCard";
 import "../../styles/home.css";
 
 
