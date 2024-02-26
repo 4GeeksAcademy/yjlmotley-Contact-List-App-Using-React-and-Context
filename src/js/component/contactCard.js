50149f659505cdc6af2b8dcf29329aa7b8084b60
Yeju Lee (Julie) Motley <149555844+yjlmotley@users.noreply.github.com>
2024-02-26 13:10:22 +0000

    using context

--- src/js/component/contactCard.js
+++ src/js/component/contactCard.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
+
 import exampleImage from "../../img/icons_user2-256.png";
 
 
 const ContactCard = () => {
+	const { full_name, email, address, phone } = contact;
+	const { actions } = useContext(Context);
+
 	return (
 		<li className="list-group-item">
 			<div className="row w-100 align-items-center">
@@ -16,33 +22,39 @@ const ContactCard = () => {
 				</div>
 				<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
 					<div className="float-end">
-						<button className="btn">
-							<i className="fas fa-pencil-alt mr-3"></i>
-						</button>
-						<button className="btn">
+						<Link to={`/editContact/${contact.id}`}>
+							<button className="btn">
+								<i className="fas fa-pencil-alt mr-3"></i>
+							</button>
+						</Link>
+						<button 
+							className="btn"
+							onClick={() => actions.deleteContacts(contact.id)}
+						>
 							<i className="fas fa-trash-alt"></i>
 						</button>
 					</div>
 					<div className="text-start">
-						<label className="name lead">Mike Anamendolla</label>
+						<label className="name lead">Mike Anamendolla {full_name}</label>
 						<br />
 						<i className="fas fa-map-marker-alt text-muted mr-3"></i>
-						<span className="text-muted">5842 Hillcrest Rd</span>
+						<span className="text-muted">5842 Hillcrest Rd {address}</span>
 						<br />
 						<span
 							className="fa fa-phone fa-fw text-muted mr-3"
 							data-toggle="tooltip"
 							title=""
 							data-original-title="(870) 288-4149"></span>
-						<span className="text-muted small">(870) 288-4149</span>
+						<span className="text-muted small">(870) 288-4149 {phone}</span>
 						<br />
 						<span
 							className="fa fa-envelope fa-fw text-muted mr-3"
 							data-toggle="tooltip"
 							data-original-title=""
-							title=""></span>
+							title="">
+						</span>
 						<span className="text-muted small text-truncate">
-							mike.ana@example.com
+							mike.ana@example.com {email}
 						</span>
 					</div>
 				</div>
--- src/js/views/editContact.js
+++ src/js/views/editContact.js
@@ -4,101 +4,110 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const EditContact = () => {
-  const { store, actions } = useContext(Context);
-  const navigate = useNavigate();
-  const params = useParams();
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+    const params = useParams();
 
-  // This stores the current contact information
-  const [contact, setContact] = useState({
-    full_name: "",
-    email: "",
-    address: "",
-    phone: "",
-  });
+    // This stores the current contact information
+    const [contact, setContact] = useState({
+        full_name: "",
+        email: "",
+        address: "",
+        phone: "",
+    });
 
-  // Need this to run for when depedencies change
-  useEffect(() => {
-    // This finds the contact in the store that matches the ID from the URL params
-    const contactData = store.contacts.find(
-      (c) => c.id === parseInt(params.id)
-    );
+    // Need this to run for when depedencies change
+    useEffect(() => {
+        // This finds the contact in the store that matches the ID from the URL params
+        const contactData = store.contacts.find(
+            (c) => c.id === parseInt(params.id)
+        );
 
-    // if the contact is found, updates the local status with their info
-    if (contactData) {
-      setContact(contactData);
-    }
-  }, [params.id, store.contacts]);
+        // if the contact is found, updates the local status with their info
+        if (contactData) {
+            setContact(contactData);
+        }
+    }, [params.id, store.contacts]);
 
   // event handler for input field changes
-  const handleChange = (e) => {
-    setContact({ ...contact, [e.target.name]: e.target.value });
-  };
+    const handleChange = (e) => {
+        setContact({ ...contact, [e.target.name]: e.target.value });
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    // this calls the editContact with the contact id and info
-    actions.editContact(params.id, contact);
+        // this calls the editContact with the contact id and info
+        actions.editContact(params.id, contact);
 
-    navigate("/");
-  };
+        navigate("/");
+    };
 
-  // edit form
-  return (
-    <div className="contact-card-container mt-0">
-      <form onSubmit={handleSubmit} className="contact-form">
-        <div className="form-group">
-          <label>Full Name</label>
-          <input
-            type="text"
-            name="full_name"
-            value={contact.full_name}
-            onChange={handleChange}
-            required
-          />
+    // edit form
+    return (
+        <div className="container">
+            <h1 className="text-center mt-5">Update Contact</h1> 
+            <form onSubmit={handleSubmit} className="contact-form">
+                <div className="form-group">
+                    <label>Full Name</label>
+                    <input
+                        type="text"
+                        name="full_name"
+                        className="form-control"
+                        value={contact.full_name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input
+                        type="email"
+                        name="email"
+                        className="form-control"
+                        value={contact.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Address</label>
+                    <input
+                        type="text"
+                        name="address"
+                        className="form-control"
+                        value={contact.address}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Phone</label>
+                    <input
+                        type="phone"
+                        name="phone"
+                        className="form-control"
+                        value={contact.phone}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                    <Link to={"/"}>
+                        <button
+                            className="btn btn-primary form-control">
+                            Cancel
+                        </button>
+                    </Link>
+                <button 
+                    type="submit"
+                    className="btn btn-primary form-control">
+                    {/* if contact.id exists, Update Contact button and vice versa*/}
+                    {contact.id ? "Update Contact" : "save"}
+                </button>
+            </form>
+            <a href="/">or get back to contacts</a>
         </div>
-        <div className="form-group">
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={contact.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Address</label>
-          <input
-            type="text"
-            name="address"
-            value={contact.address}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label>Phone</label>
-          <input
-            type="tel"
-            name="phone"
-            value={contact.phone}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="btn-group d-flex justify-content-between">
-          <Link to={"/"}>
-            <button>Back</button>
-          </Link>
-          <button type="submit">
-            {/* if contact.id exists, Update Contact button and vice versa*/}
-            {contact.id ? "Update Contact" : "Add Contact"}
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 // props for the edit contact component
